f3858da5a54fefd37a735a29dcadd99864c5a76b
mehraankush <mehraankush36@gmail.com>
2023-12-25 15:13:26 +0530

    data

---
+++ data/Footer.ts
@@ -0,0 +1,33 @@
+export const Links = [
+    {
+        title:'Home',
+        href:'/'
+    },
+    {
+        title:'Shop',
+        href:'/shop'
+    },
+    {
+        title:'About',
+        href:'/about'
+    },
+    {
+        title:'Contact',
+        href:'/contact'
+    },
+]
+
+export const Help = [
+    {
+        title:'Payment Options',
+        href:'/help'
+    },
+    {
+        title:'Returns',
+        href:'/help'
+    },
+    {
+        title:'Privacy Policies',
+        href:'/help'
+    }
+]
