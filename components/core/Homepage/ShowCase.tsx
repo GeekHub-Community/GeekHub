957c6209e495927621847e849ea92866b8117a40
mehraankush <mehraankush36@gmail.com>
2024-01-14 13:10:22 +0530

    added mouse animation

--- components/core/Homepage/ShowCase.tsx
+++ components/core/Homepage/ShowCase.tsx
@@ -1,15 +1,16 @@
 "use client"
+import { Player } from '@lottiefiles/react-lottie-player';
 import TypewriterComponent from 'typewriter-effect'
 
 const ShowCase = () => {
     return (
         <div className="HeroBack">
-            <div className="h-full w-full bg-black/70 flex justify-center items-center">
+            <div className="h-full w-full bg-black/70 flex flex-col justify-center items-center border">
 
-            <div className="flex w-full  justify-center mb-6">
-                <div className='flex-1 text-right font-black text-7xl tracking-wide	'>
-                    <h1 className='text-white mr-3'>Welcome to</h1>
-                </div>
+                <div className="flex w-full  justify-center mb-6 ">
+                    <div className='flex-1 text-right font-black text-7xl tracking-wide	'>
+                        <h1 className='text-white mr-3'>Welcome to</h1>
+                    </div>
                     <div className="text-7xl font-black  flex-1 tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-[#FFB700] to-blue/20">
                         <TypewriterComponent
                             options={{
@@ -24,6 +25,15 @@ const ShowCase = () => {
                             }}
                         />
                     </div>
+                </div>
+
+                <div className=' absolute bottom-0'>
+                    <Player
+                        autoplay
+                        loop
+                        src="/slider/mouse.json"
+                    >
+                    </Player>
                 </div>
             </div>
         </div>
