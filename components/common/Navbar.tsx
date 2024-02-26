6348352c5dbc9b399df77c007c42552c9acd357e
mehraankush <mehraankush36@gmail.com>
2024-02-26 23:17:45 +0530

    fix navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
     const [isMenu, setisMenu] = useState<boolean>(false)
     return (
 
-        <div className='flex justify-between  w-full lg:max-w-[1600px] shadow fixed top-0  bg-blue  text-black p-4 z-[1] h-16'>
+        <div className='flex justify-between  w-full lg:max-w-[1600px] shadow fixed top-0  bg-blue  text-black p-4 z-[10] h-16'>
             <div className='flex  ml-5'>
                 <Link href='/' className='flex gap-2'>
                     <Image alt='Logo' width={1000} height={1000} src='/logo.jpg' className='rounded-full border-2 border-white/70 h-10 w-10' />
@@ -37,12 +37,12 @@ export const Navbar = () => {
             <div className=' gap-2 mt-2 mr-2  md:hidden '>
                 <Sheet>
                     <SheetTrigger className='text-white'><Menu /></SheetTrigger>
-                    <SheetContent className='z-[10] '>
+                    <SheetContent className='w-[250px]'>
                         <div className=' gap-5 mt-2  flex-col flex'>
                             {
                                 Navlinks.map((link) => (
                                     <Link key={link.title} href={link.href}>
-                                        <p className='text-base text-blue font-semibold hover:text-AthensGray/80  p-1 transition-all duration-200'>{link.title}</p>
+                                        <p className='text-base text-blue font-semibold hover:text-blue/80  p-1 transition-all duration-200'>{link.title}</p>
                                     </Link>
                                 ))
                             }
