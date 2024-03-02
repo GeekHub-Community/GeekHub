608221a652d8e7bb12fb4d3d9431fc3a1b3aac48
cool-deep-96 <27581kuldeep.2021cse@gmail.com>
2024-03-02 18:20:37 +0530

    ready for main branch

--- data/Testimonials.ts
+++ data/Testimonials.ts
@@ -7,78 +7,7 @@ export const TestimonialsData = [
       profession: "Software Developer",
       thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.adipisicing aliquid architecto Codehelp Ankush as'
     },
-    {
-      id: 2,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    },
-    {
-      id: 3,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    },
-    {
-      id: 4,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    },
-    {
-      id: 5,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
-    {
-      id: 6,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
-    {
-      id: 7,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
-    {
-      id: 8,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
-    {
-      id: 9,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
-    {
-      id: 10,
-      img: "/mehra.png",
-      name: "Ankush Mehra",
-      currentCompany:'Codehelp',
-      profession: "Software Developer",
-      thoughts:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ullam deleniti error esse, debitis ratione voluptatem dignissimos possimus, cum sit animi aliquid, amet nam tempora laborum beatae neque architecto illum.'
-    }, 
+ 
     
   ];
   
